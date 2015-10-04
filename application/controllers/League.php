ae1b623405242abd430ec1a309e84f11f14882a2
Darnell Andries <dandries@my.bcit.ca>
2015-10-04 14:13:00 -0700

    added the two conferences and a better table view for league

--- application/controllers/League.php
+++ application/controllers/League.php
@@ -4,15 +4,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class League extends Application {
 	public function index()
 	{
-				$this->data['pagebody'] = 'leaguelist';    // this is the view we want shown
+		$this->data['pagebody'] = 'leaguelist';    // this is the view we want shown
         // build the list of authors, to pass on to our view
         $records = $this->leagueteams->all_afc();
 
-				foreach ($records as $record) {
-            $teams[] = array('id' => $record['id'], 'team' => $record['team']);
+		foreach ($records as $record) {
+            $afcteams[] = array('id' => $record['id'], 'team' => $record['team']);
+        }
+		$records = $this->leagueteams->all_nfc();
+
+		foreach ($records as $record) {
+            $nfcteams[] = array('id' => $record['id'], 'team' => $record['team']);
         }
 
-				$this->data['teams'] = $teams;
+		$this->data['afcteams'] = $afcteams;
+		$this->data['nfcteams'] = $nfcteams;
         $this->render();
 	}
 }
--- application/views/leaguelist.php
+++ application/views/leaguelist.php
@@ -1,5 +1,14 @@
 <div class="row">
-    {teams}
-        <div>{id} - {team}</div>
-    {/teams}
+	<h4>AFC Teams</h4>
+	<table class="table">
+		{afcteams}
+			<tr><td>{id}</td><td>{team}</td></tr>
+		{/afcteams}
+	</table>
+	<h4>NFC Teams</h4>
+	<table class="table">
+		{nfcteams}
+			<tr><td>{id}</td><td>{team}</td></tr>
+		{/nfcteams}
+	</table>
 </div>
